7ff498b048ef2eca821d8184592ed67e38bb46e5
Nguyễn Văn Minh <minhnv2306@gmail.com>
2018-11-12 00:41:48 +0700

    Fix docker,readme and migrate

--- database/migrations/2018_11_06_083146_add_column_role_table_users.php
+++ database/migrations/2018_11_06_083146_add_column_role_table_users.php
@@ -14,7 +14,7 @@ class AddColumnRoleTableUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-           $table->integer('role')->defaul(1);
+           $table->integer('role')->default(1);
         });
     }
 
@@ -26,7 +26,7 @@ class AddColumnRoleTableUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 }
